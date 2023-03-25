fb801bc308bbb497fc550b0d2dde9cd909a9632e
Yu-Chi-Chen <ylps200089@gmail.com>
2023-03-25 23:51:18 +0800

    finish read txt file and change content to binary

--- project2/asciiToBinary_readTXT.py
+++ project2/asciiToBinary_readTXT.py
@@ -2,46 +2,45 @@ import string
 import math
 
 ## read file
-f=open("project2\testFile.txt","r")
+f=open("project2\Txt_File .txt","r")
 word=f.read()
 contList=list(word)
-print(contList)
 f.close()
 
 ## change ascii(decimal) to binary
 def decimalToBinary(c:string):
     num=ord(c)
     divisor=0
-    divisor=int(divisor)
     arrList=list()
     arrList.clear()
 
     dividend=num
+    ## 沒有寫floor會出錯
     while(dividend/2!=0):
             divisor+=1
-            dividend/=2
+            dividend=math.floor(dividend/2)
     ## end while
 
     while(divisor>=0):
-            if(num>=pow(2,divisor)):
-                num-=pow(2,divisor)
+            if(num>=math.pow(2,divisor)):
+                num-=math.pow(2,divisor)
                 divisor-=1
-                arrList.append(1)
+                arrList.append('1')
             else:
                 divisor-=1
-                arrList.append(0)
+                arrList.append('0')
             ##end if-else
-
     ## end while
+
 
     while(len(arrList)<8):
             arrList.insert(0,0)
     ## end while
-    
-    print(arrList)
 
+    for i in range(0,len(arrList)):
+        print(arrList[i],end="")
+    
 
 for i in range(0,len(contList)):
   decimalToBinary(contList[i])
-  print(" ")
 ## end for
