74df163e12ee4d51f6e9d9479ff3470b5768c2bb
Yu-Chi-Chen <ylps200089@gmail.com>
2023-03-26 12:36:41 +0800

    print all pixels value

---
+++ project2/asciiToBinary_readPic.py
@@ -0,0 +1,13 @@
+from PIL import Image
+##open picture
+image=Image.open("project2\Img_File.bmp")
+##顯示L表示灰階->顯示P代表RGB
+print(image.mode)
+line_horizon=list()
+
+for i_vertical in range (image.height):
+    line_horizon=[image.getpixel((i_horizon,i_vertical))for i_horizon in range(image.width)]
+    print(*line_horizon)
+
+image.close()
+print((line_horizon))
