63e8a68fadf4ded40bfaa3741575cd72c05a527e
Yu-Chi-Chen <ylps200089@gmail.com>
2023-04-05 10:39:38 +0800

    針對pixel進行轉換成二進制確定完成，轉換為圖檔還沒完成

--- project2/asciiToBinary_readPic.py
+++ project2/asciiToBinary_readPic.py
@@ -1,27 +1,62 @@
 from def_decimalToBinary_int import decimalToBinary
 from PIL import Image
+import numpy as np
 import math
 
+'''
 ##open picture
-image=Image.open("project2\Img_File.bmp")
-path="project2\Output_BMP_binary.txt"
-f=open(path,'a')
+image=Image.open()
+path=
+ #px is a two dimension list
 
+'''
+
+with Image.open("project2/Img_File.bmp") as img:
+    img=img.convert('L')
+    img_array = np.array(img) #create two dimension matrix
+    #img_matrix=img_array.tolist() 
+#close image
+f=open("project2/Output_BMP_binary.txt",'a')
+i_vertical,i_horizon=img_array.shape
 line_horizon=list()
 imageList=list()
+line=list()
+for vertical in range (0,i_vertical):
+    for horizon in range(0,i_horizon):
+        pixelData=img_array[vertical,horizon]
+        dataBinary=decimalToBinary(pixelData)
+        for element in dataBinary:
+            print(element,end="",file=f)
+        #end for
+    #end for
+#end for
+f.close()
+
+
+'''    for horizon in range(line):
+        str="".join(decimalToBinary(str(line[vertical])))
+        print(str,end="")'''
+    #end for
+#end for
+
+'''
 for i_vertical in range (image.height):
     line_horizon=[image.getpixel((i_horizon,i_vertical))for i_horizon in range(image.width)]
+    print(len(line_horizon))
     for j in range(0,len(line_horizon)):
         str=''.join(decimalToBinary(line_horizon[j]))
-        f.write(str)
+        #f.write(str)
+        print(j,line_horizon[0],len(line_horizon))
     #end for
     str=''
 image.close()
 f.close()
+'''
 
 # call def of another program
 # decimalToBinary would return a list and input is a string
 # image_horizon is a two dimensional list
+'''
 toBinList=list()
 for i in range(0,vertical_count):
     for j in range(0,horizon_count):
@@ -34,13 +69,12 @@ for i in range(0,len(toBinList)):
         print(tempList[j],end="",file=f)
     # end for
 #end for
+'''
 
 # convert output bmp binary file into initial bmp file
 path="project2\Output_BMP_binary.txt"
-bmpBinary=list()
 with open(path,'r') as f:
-    for i in f:
-       bmpBinary.append(i)
+    bmpBinary=f.read
 
 str="".join(bmpBinary)
 bmpBinary=list()
@@ -75,3 +109,7 @@ for i in new_data:
 a= bytearray(int_data)
 f.write(a)
 f.close()  
+
+
+
+
